4fa279f0cb0a1bbefefd9070fb19c8e1d5861f19
pckerneis <pc.kerneis@gmail.com>
2023-12-29 23:18:34 +0100

    Add GitHub action for site publication

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Documenté",
+  base: "/documente/",
   description: "A literate testing framework to generate automated tests from documentation files",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
